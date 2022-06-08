7d68d17eedffdb5986f330b88f215ac9bc7f6b0e
Darmimese <darmimesseleseyoum69@gmail.com>
2022-06-08 11:06:59 -0700

    queen

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ * Description- a program to print alphabet 10times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int x = 0;
+		while (x < 10)
+		{
+		alpha = 'a';
+			while (alpha <= 'z')
+			{
+				_putchar(alpha);
+				alpha++;
+			}
+			_putchar('\n');
+			x++;
+		}
+}
