895db5d371c234117c1bd9357bd54d733da18671
Darmimese <darmimesseleseyoum69@gmail.com>
2022-06-09 12:51:34 -0700

    love

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_most_numbers - prints numbers between 0 to 9
  * (neither 2 nor 4).
- * Return: no return.
+ * Return: Always 0.
  */
+int main(void)
 void print_most_numbers(void)
 {
   int ch;
