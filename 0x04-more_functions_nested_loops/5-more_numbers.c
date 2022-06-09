2bf2ff6341364d4cbb76cee9180bdd0d764e7376
Darmimese <darmimesseleseyoum69@gmail.com>
2022-06-09 14:16:38 -0700

    road

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,21 +2,20 @@
 
 /**
  * more_numbers - prints numbers between 0 to 14
- * 10 times.
+ * print 10 times.
  * Return: no return.
  */
 void more_numbers(void)
 {
-   char c;
-   int i = 0;
+  int i, ch;
 
   for (i = 0; i < 10; i++)
   {
-    for (ch = 0; ch <= 14; ch++)
+    for (ch = 0; ch < 15; ch++)
     {
       if (ch >= 10)
-        _putchar((ch / 10) + '0');
-        _putchar((ch % 10) + '0');
+        _putchar((ch / 10) + 48);
+        _putchar((ch % 10) + 48);
     }
     _putchar('\n');
   }
