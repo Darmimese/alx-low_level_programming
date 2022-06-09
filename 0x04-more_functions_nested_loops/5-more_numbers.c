54f06e78cb957444a82d69402e0c5921f6d5da26
Darmimese <darmimesseleseyoum69@gmail.com>
2022-06-09 14:27:51 -0700

    queen

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * more_numbers - a function that prints 10 times the numbers, from 0 to 14,
+ * main - a function that prints 10 times the numbers, from 0 to 14,
  * followed by a new line.
  * Return: no return.
  */
