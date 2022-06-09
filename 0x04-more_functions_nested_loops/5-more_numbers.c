ae157524492cc71cf0e42813536eb8bf830cf89d
Darmimese <darmimesseleseyoum69@gmail.com>
2022-06-09 14:13:25 -0700

    queen

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,16 @@
  */
 void more_numbers(void)
 {
-  int i, ch;
+   char c;
+   int i = 0;
 
   for (i = 0; i < 10; i++)
   {
-    for (ch = 0; ch < 15; ch++)
+    for (ch = 0; ch <= 14; ch++)
     {
       if (ch >= 10)
-        _putchar((ch / 10) + 48);
-      _putchar((ch % 10) + 48);
+        _putchar((ch / 10) + '0');
+        _putchar((ch % 10) + '0');
     }
     _putchar('\n');
   }
