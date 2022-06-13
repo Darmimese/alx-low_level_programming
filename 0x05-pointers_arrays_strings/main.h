770bc11a61851943edc1750b83bbff9a88d80487
Darmimese <darmimesseleseyoum69@gmail.com>
2022-06-13 12:03:45 -0700

    queen

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endof /* MAIN_H */
+#endif /* MAIN_H */
