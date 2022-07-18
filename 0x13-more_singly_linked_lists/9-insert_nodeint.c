92c1ee7943f33824e7a43d8c430d222a212691cb
Darmimese <darmimesseleseyoum69@gmail.com>
2022-07-18 12:43:20 -0700

    queen

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,43 @@
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - A function that inserts node at given index
- * @head: A pointer to listint_t structure
- * @idx: The index of the list
- * @n: An integer data for new node
- * Return: The address to new node at specified index, or NULL if it failed
- */
+  * insert_nodeint_at_index - inserts a new node at a given position.
+  * @head: pointer to pointer of the first element on list.
+  * @idx: index of list where the new node should be added.
+  * @n: integer to be inserted.
+  *
+  * Return: address of the new node or NULL if it fails.
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
 	listint_t *new_node, *temp;
-
-	if (head == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		new_node->n = n;
-		return (new_node);
-	}
-
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		new_node->n = n;
-		*head = new_node;
-		return (new_node);
-	}
+	unsigned int count;
 
 	temp = *head;
-	while (i < idx)
+
+	count = 0;
+	while (temp && count < idx - 1)
 	{
 		temp = temp->next;
-		i++;
+		count++;
 	}
 
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		if (idx == 0)
+		{
+			new_node->next = *head;
+			*head = new_node;
+			return (new_node);
+		}
+		if (count + 1 == idx)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+	}
+	free(new_node);
+	return (NULL);
 }
